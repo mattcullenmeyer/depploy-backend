eb1d47cd9c0fffed2504fadbf4d7006feaba35c4
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-11-03 15:49:49 -0600

    [depploy-37] CI/CD Pipeline

--- router/routes.go
+++ router/routes.go
@@ -11,7 +11,7 @@ func RegisterRoutes() *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/test", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "success"})
+		c.JSON(http.StatusOK, gin.H{"message": "new deployment"})
 	})
 
 	user := router.Group("/user")
