a6ba375ef57531de7d06349289b7be9d6d309043
mattcullenmeyer <mattcullenmeyer@gmail.com>
2023-01-07 10:21:54 -0700

    [depploy-45] Email verification

--- pkg/utils/get_parameter.go
+++ pkg/utils/get_parameter.go
@@ -1,17 +1,23 @@
 package utils
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
-func GetParameter(name string) (string, error) {
+func GetParameter(parameter string) (string, error) {
 	sess, err := session.NewSession()
 	if err != nil {
 		return "", err
 	}
 	svc := ssm.New(sess)
+
+	environment := os.Getenv("ENVIRONMENT")
+	name := fmt.Sprintf("/%s/depploy/backend/%s", environment, parameter)
 
 	output, err := svc.GetParameter(
 		&ssm.GetParameterInput{
